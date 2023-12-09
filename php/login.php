3666e1c099039b80dc2d37a0821893198e64f797
DulinaSH2001 <dulinahejithaindrawansha@gmail.com>
2023-12-09 12:57:55 +0530

    create login

--- php/login.php
+++ php/login.php
@@ -5,44 +5,30 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $tempusername = $_POST['email'];
     $temppassword = $_POST['password'];
 
-    if ($tempusername === 'apppluseadmin665@gmail.com' && $temppassword === 'admin@2023app') {
+    if ($tempusername == 'adminsaradhalanka@gmail.com' && $temppassword == 'admin@2023saradhalanka') {
         header('Location:head admin\adminDashboard.php');
-        
 
-    } else if ($tempusername === 'appplusesecurity665@gmail.com' && $temppassword === 'security@2023app') {
-        header('Location:Security admin\securityadminDashboard.php');
-       
-
-
-    } else if ($tempusername === 'appplusepayment665@gmail.com' && $temppassword === 'payment@2023app') {
-        header('Location:Payment Admin\paymentadminDashboard.php');
-        
 
     } else {
 
-        $sql = "SELECT * FROM users WHERE (Username = '$tempusername' OR Email = '$tempusername') AND (Password = '$temppassword');";
+        $sql = "SELECT * FROM users WHERE (username = '$tempusername' OR email = '$tempusername') AND (password = '$temppassword');";
         $result = mysqli_query($connect, $sql);
 
-        if ($result && $result->num_rows === 1) {
+        if ($result && $result->num_rows == 1) {
             session_start();
-            $d = $result->fetch_assoc();
-            $_SESSION['u'] = $d;
+            $user = $result->fetch_assoc();
+            $_SESSION['u'] = $user;
 
             setcookie("username", $tempusername, time() + (60 * 60 * 24 * 365));
             setcookie("password", $temppassword, time() + (60 * 60 * 24 * 365));
 
-            if ($_SESSION['u']['usertype'] === 'User') {
-                header("Location: homepage.php");
-                exit();
 
-            } else {
-                header("Location: devprofile.php");
-                exit();
-            }
+            header("Location: userprofile.php");
+
 
 
         } else {
-            echo "Invalid username or password.";
+            echo "<script> alert('invalide user name or password')</script>";
         }
     }
 }
@@ -50,7 +36,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 if (isset($_GET['signup'])) {
     $signupcommand = $_GET['signup'];
     if ($signupcommand === 'Signup') {
-        header("Location: Registation.php");
+        header("Location: Signup.php");
         exit();
     }
 }
@@ -65,110 +51,7 @@ if (isset($_GET['signup'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
 
-    <style>
-        body {
-            margin: 0;
-            padding: 0;
-            font-family: Arial, sans-serif;
-        }
 
-        .login-fullframe {
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            height: 100vh;
-            background-color: #f5f5f5;
-        }
-
-        .login-form {
-            width: 400px;
-            padding: 20px;
-            background-color: #fff;
-            border-radius: 8px 0px 0px 8px;
-            box-shadow: 0px 1px 10px rgba(0, 0, 0, 0.2)
-        }
-
-        .login-form h2 {
-            margin: 0 0 20px;
-            text-align: center;
-            color: #333;
-        }
-
-        .plch input[type="text"],
-        .plch input[type="password"] {
-            width: 90%;
-            padding: 10px;
-            margin-bottom: 15px;
-            border: 1px solid #ccc;
-            border-radius: 4px;
-
-        }
-
-        .btn1 input[type="submit"] {
-            background: linear-gradient(to right, lightpurple, lightyellow);
-
-        }
-
-        .rightside-container {
-            width: 400px;
-            display: flex;
-            height: 394px;
-            padding: 20px;
-            border-radius: 8px;
-            border-radius: 0px 8px 8px 0px;
-            box-shadow: 0px 1px 10px rgba(0, 0, 0, 0.2);
-            background: linear-gradient(to right, #439e96b0, #08154c);
-            flex-direction: column;
-            flex-wrap: nowrap;
-            justify-content: center;
-            align-items: center;
-        }
-
-
-
-        .btn1 input[type="submit"] {
-            width: 80%;
-            padding: 11px;
-            background-color: #08154c;
-            color: white;
-            border: none;
-            border-radius: 26px;
-            cursor: pointer;
-        }
-
-        .btn1 input[type="submit"]:hover{
-            width: 80%;
-            padding: 11px;
-            background-color: #08374c;
-            color: white;
-            border: none;
-            border-radius: 26px;
-            cursor: pointer;
-        }
-
-
-
-        .rightside-container h4 {
-            margin: 0 0 10px;
-            font-size: 24px;
-            color: #fff;
-        }
-
-        .rightside-container h6 {
-            margin: 0;
-            font-size: 14px;
-            color: #fff;
-        }
-
-        .btn1 {
-            display: flex;
-            flex-direction: column;
-            flex-wrap: nowrap;
-            align-content: center;
-            justify-content: center;
-            align-items: center;
-        }
-    </style>
 
 
 </head>
