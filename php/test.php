fa335c7d1d1d477bbe38197f71f854df5baa39b6
DulinaSH2001 <dulinahejithaindrawansha@gmail.com>
2023-12-09 10:57:01 +0530

    create update profile php

--- php/test.php
+++ php/test.php
@@ -1,8 +1,36 @@
-<?php 
-$email ="dulinahejitha@gmail.com";
-$pass = "Dulina!123";
+<!DOCTYPE html>
+<html lang="en">
 
-echo " hello email is {$email} <br>";
-echo "";
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
 
-?>
+<body>
+
+<!-- calcilater -->
+
+  <!-- <form action="test.php" method="get">
+    <label for="price ">price:</label><br>
+    <input type="text" name="price">
+    <label for="qty">qty</label>
+    <input type="text" name="qty" >
+
+    <input type="submit"> catculate price </input>
+  </form> -->
+  <?php
+        // $price = $_GET["price"];
+        // $qty = $_GET["qty"];
+        // $total = $price * $qty;
+
+        // echo "<br> total price:{$total} <br>";
+  ?>
+
+
+
+
+
+</body>
+
+</html>
--- php/userprofile.php
+++ php/userprofile.php
@@ -1,24 +1,83 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
-    <form action="">
-    <input type="text" value="" name="Fname">
-    <input type="text" value="" name="Lname" >
-    <input type="text" value="" name="email">
-    <input type="text" value=""  name="username">
-    <input type="text" value="" name="">
-    <input type="text" value="" >
+
+
+<?php
+    include 'connect.php';
+    session_start();
+
+   
+
+
+    $id = $_SESSION['u']['ID'];
+
+   
+
+    // Fetch the user's information from the database
+    $sql = "SELECT * FROM users WHERE ID = '$id'";
+    $result = $connect->query($sql);
+    $row = $result->fetch_assoc();
+
+    $fname = $row["FName"];
+    $lname = $row["LName"];
+    $email = $row["Email"];
+    $username = $row["Username"];
+    $password = $row["Password"];
+    $dob = $row["DOB"];
+    $tel = $row["Phonenumber"];
+    ?>
+    <form action="userprofile" method="post">
+        <input type="text" value="" name="Fname">
+        <input type="text" value="" name="Lname">
+        <input type="text" value="" name="email">
+        <input type="text" value="" name="username">
+        <input type="text" value="" name="address">
+        <input type="text" value="" name="password">
+        <button type="submit" class="btn btn-primary">Update Profile</button>
+        <button type="reset">Cancel</button>
+<?php 
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // Retrieve and sanitize the updated form data
+    $firstname = $_POST['firstname'];
+    $lastname = $_POST['lastname'];
+    $email = $_POST['email'];
+    $DOB = $_POST['dob'];
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    $tel = $_POST['tel'];
+
+    // Update the user's profile in the database
+    $sql = "UPDATE users SET FName = '$firstname', LName = '$lastname', Phonenumber = '$tel', Email = '$email', Password = '$password', Username = '$username', DOB = '$DOB' WHERE ID = '$id'";
+
+    if (mysqli_query($connect, $sql)) {
+        header("Location: userprofile.php");
+        exit();
+    } else {
+        echo "Error: " . mysqli_error($connect);
+    }
+}?>
+
+
+
+
 
 
 
     </form>
 
+   
 
-    
+
+
 </body>
+
 </html>
